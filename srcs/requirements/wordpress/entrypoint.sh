8ba28e1cd22ae3b5055008528615142a9d57abc3
amoutill <contact@amoutillon.com>
2024-11-14 04:26:01 +0100

    Fix wordpress installation into wordpress data volume

---
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ ! -d /var/www/wordpress ]; then
+  echo "Wordpress not found, installing..."
+  mkdir -p /var/www
+  cd /var/www
+  echo "Downloading Wordpress archive..."
+  wget -q https://wordpress.org/latest.tar.gz
+  echo "Extracting Wordpress archive..."
+  tar -xzf latest.tar.gz
+  rm latest.tar.gz
+  chown -R php:www-data .
+  echo "Wordpress installed successfully!"
+fi
+
+echo "Starting PHP-FPM..."
+su-exec php $@
