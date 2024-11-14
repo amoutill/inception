e8f072847fc248f8cc7a409ab7fd81e4f71c9760
amoutill <contact@amoutillon.com>
2024-11-14 20:02:22 +0100

    Fix permissions bug for nginx cert

--- srcs/requirements/nginx/entrypoint.sh
+++ srcs/requirements/nginx/entrypoint.sh
@@ -3,5 +3,9 @@
 mkdir -p /run/nginx
 chown -R nginx:nginx /run/nginx
 
+mkdir -p /run/ssl
+cp /run/secrets/ssl_* /run/ssl
+chown -R nginx:nginx /run/ssl
+
 echo "Starting Nginx..."
 su-exec nginx "$@"
